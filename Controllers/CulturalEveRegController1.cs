ee397ed8108e47b453e36ea141bf026fa1dd629b
Pavan Balaji <pavanbalaji1999@gmail.com>
2022-06-20 15:55:45 +0530

    second commit

---
+++ Controllers/CulturalEveRegController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CulturalEventRegistrationApp.Controllers
+{
+    public class CulturalEveRegController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            return View();
+        }
+    }
+}
---
+++ Models/EveRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CulturalEventRegistrationApp.Models
+{
+    public class EveRegistration
+    {
+        public long UserId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string ParticipateIn { get; set; }
+        public DateTime DateOn { get; set; }
+    }
+}
